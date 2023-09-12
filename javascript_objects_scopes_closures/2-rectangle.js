6f25a1bd72b2a0bf4f0620a289c7303ef8e849ed
geachgithub <getachewamogne4@gmail.com>
2023-09-11 18:37:47 -0900

    class manipulation

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,12 @@ class Rectangle{
     height
     width
     constructor(w,h){
+        if(w<=0 || h<=0){
+        }
+        else{
             this.height=h
             this.width=w    
-        
+        }
             }
 }
 /*r1=new Rectangle(2,9)
@@ -13,6 +16,6 @@ console.log(r1)
 r2=new Rectangle(2,0)
 console.log(r2)
 r3=new Rectangle(2,-9)
-console.log(r3)
+console.log(r3)*/
 
-module.exports=Rectangle*/
+module.exports=Rectangle
