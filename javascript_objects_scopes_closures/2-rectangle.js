fc59c0a561a9fba3e9306bc8603cf30955eba930
geachgithub <getachewamogne4@gmail.com>
2023-09-11 18:18:25 -0900

    class manipulation

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+class Rectangle{
+    height
+    width
+    constructor(w,h){
+        if(w<=0){
+            this.height=''
+            this.width=''
+        }
+        else{
+            this.height=h
+            this.width=w    
+        }
+            }
+}
+module.exports=Rectangle
