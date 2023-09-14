bd7b17fe73ef39e91d6b44c682cf8b9189853f50
geachgithub <getachewamogne4@gmail.com>
2023-09-13 22:24:07 -0900

    callback function

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,13 +1,12 @@
 
 //javascript callback functions
-function callMeMoby(number, fun){
+export function callMeMoby(number, fun){
     i=0
     while(number>i){
         fun()
         i++
     }
 }
-module.exports=callMeMoby
 /*callMeMoby(3,function fun(){
     console.log('c is fun')
     } 
